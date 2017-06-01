b729965e2537fa7d41ff7e3ea2ac55662e7fa80f
赵楠 <2205567926@qq.com>
2017-06-01 21:28:29 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,12 +1,9 @@
 module.exports = main
 function main(email, suffixes) {
-  var index = email.lastIndexOf("@");
-  var ext = email.substr(index+1);
-  if(ext == suffixes) {
+  var mail = email.split("@")[1];
+  if(mail == suffixes) {
     return true;
   }
   else return false;
   // Write your code here
 };
-
-console.log(main('tom@qq.com', ['qq.com']))
